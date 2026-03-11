6c12187a4780d0fe5bfbd03411aa047c35998a3e
אביאל קדם <avielkedem2@gmail.com>
2026-03-11 14:32:39 +0200

    רשימה מחלקות ותורים חלק ב

--- hospital_mgmt.py
+++ hospital_mgmt.py
@@ -1,4 +1,3 @@
-import random
 def list_of_dept(departments):
     """"בניתי לולא שעוברת על הרשימה של המחלקות
     ומדפיסה לי אותם בצורה שלכול מחלקה יש מספר """
@@ -12,48 +11,11 @@ def list_of_dept(departments):
     while True:
         choice = int(input("Enter a choice "))
         count = 1
-        while count != 9:
+        while count != len(departments) + 1:
             if choice == count:
                 return departments[count - 1]
             count += 1
         print("please enter a number from the list")
-
-
-
-def queue_number(departments, numbers, dept):
-    """"הפונקציה מחזירה מספר תור לפי המחלקה"""
-
-    if dept == departments[0]:
-        queue_num = f"A{numbers}"
-        return queue_num
-
-    elif dept == departments[1]:
-        queue_num = f"B{numbers}"
-        return queue_num
-
-    elif dept == departments[2]:
-        queue_num = f"C{numbers}"
-        return queue_num
-
-    elif dept == departments[3]:
-        queue_num = f"D{numbers}"
-        return queue_num
-
-    elif dept == departments[4]:
-        queue_num = f"E{numbers}"
-        return queue_num
-
-    elif dept == departments[5]:
-        queue_num = f"F{numbers}"
-        return queue_num
-
-    elif dept == departments[6]:
-        queue_num = f"G{numbers}"
-        return queue_num
-
-    elif dept == departments[7]:
-        queue_num = f"H{numbers}"
-        return queue_num
 
 
 
@@ -68,5 +30,88 @@ hospital_departments = [
     "Oncology",          # אונקולוגיה
     "Intensive Care"     # טיפול נמרץ
 ]
-numbers_list = random.randint(10,50)
-print(queue_number(hospital_departments, numbers_list, list_of_dept(hospital_departments)))
+
+
+def write_numbers_list():
+    """"הפונקציה יוצרת קובץ טקסט עם מספר בשביל התור.
+    אני החלטתי שהמספר הרשוני הוא 10 כי ככה היה בלי"""
+    try:
+        numbers = 10
+        with open("list_of_numbers.txt", "w", encoding="utf-8") as f:
+            f.write(str(numbers))
+
+    except Exception as E:
+        print(E)
+
+""""בסוף צריך לתת תנאי להפעלה של הפונקציה"""
+write_numbers_list()
+
+
+
+def numbers_list():
+    """"הפונקציה מחזירה את המספר שיש בקובץ טקסט."""
+    try:
+        with open("list_of_numbers.txt", "r") as f:
+            r = f.readlines()
+            return r[0]
+    except Exception as E:
+        print(E)
+
+
+
+def new_number(num):
+    """"הפונקציה מקבלת את המספר שיש בקובץ טקסט ומוסיפה לו 1+ ."""
+    try:
+        with open("list_of_numbers.txt", "w") as f:
+            n = int(num) + 1
+            f.write(str(n))
+    except Exception as E:
+        print(E)
+
+
+
+def queue_number(departments, numbers, dept):
+    """"הפונקציה מחזירה מספר תור לפי המחלקה"""
+
+    if dept == departments[0]:
+        queue_num = f"A{numbers}"
+        new_number(numbers)
+        return queue_num
+
+    elif dept == departments[1]:
+        queue_num = f"B{numbers}"
+        new_number(numbers)
+        return queue_num
+
+    elif dept == departments[2]:
+        queue_num = f"C{numbers}"
+        new_number(numbers)
+        return queue_num
+
+    elif dept == departments[3]:
+        queue_num = f"D{numbers}"
+        new_number(numbers)
+        return queue_num
+
+    elif dept == departments[4]:
+        queue_num = f"E{numbers}"
+        new_number(numbers)
+        return queue_num
+
+    elif dept == departments[5]:
+        queue_num = f"F{numbers}"
+        new_number(numbers)
+        return queue_num
+
+    elif dept == departments[6]:
+        queue_num = f"G{numbers}"
+        new_number(numbers)
+        return queue_num
+
+    elif dept == departments[7]:
+        queue_num = f"H{numbers}"
+        new_number(numbers)
+        return queue_num
+
+
+print(queue_number(hospital_departments, numbers_list(), list_of_dept(hospital_departments)))
